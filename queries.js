c03b4a79bcfb210c7ddeea35c46f78acf9f4664f
michelle <michzzzm@gmail.com>
2019-12-13 20:31:50 -0600

    Clean up queries.js
    - Replaced disease parentheses parsing with regex
    - Made shared counter function

--- queries.js
+++ queries.js
@@ -1,94 +1,56 @@
 const db = require('./db');
 
+const sanitizeDiseaseName = (name) => name.replace(/\(.*\)/g, '').trim();
+
+const countOccurrences = (rows, key) => rows.reduce((acc, row) => {
+  const prop = row[key];
+  acc[prop] = acc[prop] ? acc[prop] + 1 : 1;
+  return acc;
+}, {});
+
+const countsToArray = (counts) => Object.keys(counts).map((key) => [key, counts[key]]);
+
 function getDiseases (rows) {
     // TODO: match with master disease list
-    var diseases = new Map();
+    const cleanedRows = rows.map((row) => ({
+      ...row,
+      disease: sanitizeDiseaseName(row.disease),
+    }));
 
-    for (const row of rows) {
-      end_index = row.disease.indexOf('(') - 1;
-      general_disease = row.disease;
-
-      if (end_index != -2) {
-        general_disease = general_disease.substring(0, end_index);
-      }
-
-      if ( !diseases.has(general_disease)) {
-        diseases.set(general_disease, 0);
-      }
-
-      diseases.set(general_disease, diseases.get(general_disease) + 1);
-      // console.log(row.disease);
-      // console.log(diseases.get(row.disease));
-    }
-
-    return Array.from(diseases);
+    return countsToArray(countOccurrences(cleanedRows, 'disease'));
 }
 
 
 function getPlatforms (rows) {
   // TODO: match with master disease list
-  var instruments = new Map();
+  const counts = countOccurrences(rows, 'instrument');
+  counts['Illumina HiSeq 2500'] = 0;
+  counts['Illumina HiSeq 4000'] = 0;
+  counts['HiSeq X Ten'] = 0;
+  counts['NovaSeq 6000'] = 0;
+  counts['NextSeq 500'] = 0;
 
-  for (const row of rows) {
-    if ( !instruments.has(row.instrument)) {
-      instruments.set(row.instrument, 0);
-    }
-
-    instruments.set(row.instrument, instruments.get(row.instrument) + 1);
-    // console.log(row.disease);
-    // console.log(diseases.get(row.disease));
-  }
-
-  instruments.set('Illumina HiSeq 2500', 0);
-  instruments.set('Illumina HiSeq 4000', 0);
-  instruments.set('HiSeq X Ten', 0);
-  instruments.set('NovaSeq 6000', 0);
-
-  instruments.set('NextSeq 500', 0);
-
-
-
-  return Array.from(instruments);
+  return countsToArray(counts);
 }
 
 
 function getLibraryLayouts (rows) {
     // TODO: match with master disease list
-    var libraryLayouts = new Map();
+    const counts = countOccurrences(rows, 'library_format');
+    counts['SINGLE'] = 0;
 
-    for (const row of rows) {
-      if ( !libraryLayouts.has(row.library_format)) {
-        libraryLayouts.set(row.library_format, 0);
-      }
-
-      libraryLayouts.set(row.library_format, libraryLayouts.get(row.library_format) + 1);
-      // console.log(row.disease);
-      // console.log(diseases.get(row.disease));
-    }
-
-    libraryLayouts.set('SINGLE', 0)
-
-    return Array.from(libraryLayouts);
+    return countsToArray(counts);
 }
 
 
 function getReadLengths (rows) {
   // TODO: match with master disease list
-  var readLengths = new Map();
+  return countsToArray(countOccurrences(rows, 'read_length'));
+}
 
-  for (const row of rows) {
-
-
-    if ( !readLengths.has(row.read_length)) {
-     readLengths.set(row.read_length, 0);
-    }
-
-    readLengths.set(row.read_length, readLengths.get(row.read_length) + 1);
-
-    //readLengthsList.push (readLength);
-  }
-  return Array.from(readLengths);
-
+function getTissues (rows) {
+  // TODO: match with master disease list
+  return countsToArray(countOccurrences(rows, 'tissue'));
 }
 
 const getData = (request, response) => {
@@ -98,42 +60,25 @@ const getData = (request, response) => {
       }
 
       // get each sample
-      var samplesList = [];
-
-      for (const row of results.rows) {
-
-
-        end_index = row.disease.indexOf('(') - 1;
-        general_disease = row.disease;
-  
-        if (end_index != -2) {
-          general_disease = general_disease.substring(0, end_index);
-        }
-
-        var sample = {
-          sample_name: row.sample_name,
-          sra_id: row.sra_id,
-          doi: row.doi,
-          link: row.link,
-          age: row.age,
-          sex: row.sex,
-          library_format: row.library_format,
-          platform: row.instrument,
-          read_length: row.read_length,
-          datatype: row.assay_type,
-          disease: general_disease,
-        };
-        samplesList.push (sample);
-      }
-
-
+      const samplesList = results.rows.map((row) => ({
+        sample_name: row.sample_name,
+        sra_id: row.sra_id,
+        doi: row.doi,
+        link: row.link,
+        age: row.age,
+        sex: row.sex,
+        library_format: row.library_format,
+        platform: row.instrument,
+        read_length: row.read_length,
+        datatype: row.assay_type,
+        disease: sanitizeDiseaseName(row.disease),
+      }));
 
       // get metadata stats for all samples
-      var diseaseList = getDiseases (results.rows);
-      var platformList = getPlatforms (results.rows);
-      var libraryLayoutList = getLibraryLayouts (results.rows);
-      var readLengthList = getReadLengths (results.rows);
-
+      const diseaseList = getDiseases (results.rows);
+      const platformList = getPlatforms (results.rows);
+      const libraryLayoutList = getLibraryLayouts (results.rows);
+      const readLengthList = getReadLengths (results.rows);
 
       // send data
       response.status(200).json({
@@ -156,25 +101,7 @@ const getPublications = (request, response) => {
     }
 
 
-    var publicationsList = [];
-
-    for (const row of results.rows) {
-      var publication = {
-        author: row.author,
-        // TODO FIX DATE
-        date: row.date,
-        journal: row.journal,
-        pmid: row.pmid,
-        doi: row.doi,
-        link: row.link,
-        title: row.title,
-        datastore: row.datastore,
-      };
-
-      publicationsList.push(publication);
-    }
-
-    console.log(publicationsList);
+    const publicationsList = results.rows
 
     response.status(200).json({
       publications: publicationsList,
