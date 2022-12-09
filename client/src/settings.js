64957a7bd0644cc4f14e48e01fa56bb599af8772
Haizi Zheng <haizi.zh@gmail.com>
2022-12-08 19:21:24 -0500

    Set the public S3 URI through env

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,8 +1,4 @@
-export const s3Prefix =
-  'https://s3.us-east-2.amazonaws.com/cfdnadb.epifluidlab.cchmc.org/entries';
-
-export const s3Bucket =
-  'https://s3.us-east-2.amazonaws.com/cfdnadb.epifluidlab.cchmc.org';
+export const s3Bucket = '/data';
 
 export const showBAM = false;
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 // const db = require('./queries')
 
-const { seqrun, publication, summary } = require('./routes');
+const { seqrun, publication, summary, s3public, misc } = require('./routes');
 
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/api/v1/seqrun', seqrun);
 app.use('/api/v1/publication', publication); // http://localhost:3000/visualization/c91952a9b48f32a8f26a.worker.js
 app.use('/api/v1/summary', summary);
+app.use('/api/v1/misc', misc);
+app.use('/data', s3public);
 
 WORKER_SCRIPTS = {}
 
